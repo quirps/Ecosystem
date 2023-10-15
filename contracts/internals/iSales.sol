b44e493b93021239fe48bff3566491e329cd65f1
quirp <joesters@msn.com>
2023-10-14 19:44:09 -0700

    sales diamond refactoring

--- contracts/internals/iSales.sol
+++ contracts/internals/iSales.sol
@@ -1,41 +1,78 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "../libraries/utils/Ownable.sol";
+import "../internals/ERC1155/iERC1155Transfer.sol";
 import "../libraries/LibSales.sol";
 
-contract iSales is iERC1155Transfer {
+contract SalesMainFacet is Ownable, iERC1155Transfer {
+    using SalesLib for SalesLib.Sale;
+
     event SaleCreated(uint256 saleId);
     event ItemPurchased(uint256 saleId, address buyer, uint256 numBundles);
 
-    function _createSale(uint256 saleId, LibSales.Sale memory saleData, uint256 predecessorSaleId) internal {
-        LibSales.SalesStorage storage sstore = LibSales.salesStorage();
-        
+    function createTieredSales(SalesLib.Sale[] calldata salesData) external onlyOwner {
+        SalesLib.SalesStorage storage ss = SalesLib.salesStorage();
+
+        for (uint256 i = 0; i < salesData.length; i++) {
+            uint256 predecessorSaleId = (i == 0) ? 0 : ss.salesCounter;
+            createSale(ss.salesCounter + 1, salesData[i], predecessorSaleId);
+            ss.salesCounter++;
+        }
+    }
+
+    function viewSale(uint256 saleId) external view returns (SalesLib.Sale[] memory) {
+        return _retrieveSaleAndPredecessors(saleId);
+    }
+
+    function buyItems(uint256 saleId, uint256 numBundles) external {
+        _validatePurchase(saleId, numBundles);
+
+        SalesLib.Sale memory sale = SalesLib.getSale(saleId);
+        uint256 totalPrice = sale.paymentAmount * numBundles;
+
+        _safeTransferFrom(msg.sender, owner(), sale.paymentTokenId, totalPrice, "");
+
+        for (uint i = 0; i < sale.itemIds.length; i++) {
+            uint256 itemId = sale.itemIds[i];
+            uint256 itemAmount = sale.itemAmounts[i] * numBundles;
+            _safeTransferFrom(owner(), msg.sender, itemId, itemAmount, "");
+        }
+
+        SalesLib.setSaleStats(saleId, msg.sender, numBundles);
+
+        emit ItemPurchased(saleId, msg.sender, numBundles);
+    }
+
+    function createSale(uint256 saleId, SalesLib.Sale memory saleData, uint256 predecessorSaleId) internal {
         require(saleData.itemIds.length == saleData.itemAmounts.length, "Mismatched item data");
         require(saleData.endTime > saleData.startTime, "Invalid time range");
 
-        saleData.predecessorSaleId = predecessorSaleId; 
-        sstore.sales[saleId] = saleData;
+        saleData.predecessorSaleId = predecessorSaleId;
+
+        SalesLib.setSale(saleId, saleData);
 
         emit SaleCreated(saleId);
     }
 
-    function _retrieveSaleAndPredecessors(uint256 saleId) internal view returns (LibSales.Sale[] memory) {
-        LibSales.SalesStorage storage sstore = LibSales.salesStorage();
-        
-        require(sstore.sales[saleId].endTime > 0, "Nonexistent sale");
+    function _retrieveSaleAndPredecessors(uint256 saleId) internal view returns (SalesLib.Sale[] memory salesList) {
+        SalesLib.Sale memory sale = SalesLib.getSale(saleId);
+
+        require(sale.endTime > 0, "Nonexistent sale");
 
         uint256 maxPredecessors = 100;
-        LibSales.Sale[] memory salesList = new LibSales.Sale[](maxPredecessors);
+        salesList = new SalesLib.Sale[](maxPredecessors);
         uint256 count = 0;
 
         while (saleId != 0 && count < maxPredecessors) {
-            salesList[count] = sstore.sales[saleId];
-            saleId = sstore.sales[saleId].predecessorSaleId;
+            salesList[count] = sale;
+            saleId = sale.predecessorSaleId;
+            sale = SalesLib.getSale(saleId);
             count++;
         }
 
         // Resize array to match the actual count
-        LibSales.Sale[] memory trimmedSalesList = new LibSales.Sale[](count);
+        SalesLib.Sale[] memory trimmedSalesList = new SalesLib.Sale[](count);
         for (uint256 i = 0; i < count; i++) {
             trimmedSalesList[i] = salesList[i];
         }
@@ -44,16 +81,17 @@ contract iSales is iERC1155Transfer {
     }
 
     function _validatePurchase(uint256 saleId, uint256 numBundles) internal view {
-        LibSales.SalesStorage storage sstore = LibSales.salesStorage();
-        
-        LibSales.Sale memory sale = sstore.sales[saleId];
+        SalesLib.Sale memory sale = SalesLib.getSale(saleId);
+        uint256 userSaleStats = SalesLib.getSaleStats(saleId, msg.sender);
 
         require(block.timestamp >= sale.startTime && block.timestamp <= sale.endTime, "Sale not active");
-        require(sstore.memberRank[msg.sender] >= sale.rankRequired, "Insufficient rank");
-        require(sstore.saleStats[saleId][msg.sender] + numBundles <= sale.limit, "Purchase limit exceeded");
+        require(SalesLib.salesStorage().memberRank[msg.sender] >= sale.rankRequired, "Insufficient rank");
+        require(userSaleStats + numBundles <= sale.limit, "Purchase limit exceeded");
 
         if (sale.predecessorSaleId != 0) {
-            require(sstore.saleStats[sale.predecessorSaleId][msg.sender] > 0, "Must purchase from predecessor sale first");
+            require(SalesLib.getSaleStats(sale.predecessorSaleId, msg.sender) > 0, "Must purchase from predecessor sale first");
         }
     }
+
+    // ... other functions
 }
--- contracts/libraries/LibSales.sol
+++ contracts/libraries/LibSales.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-library LibSales {
+library SalesLib {
     bytes32 constant STORAGE_SALES = keccak256("diamond.storage.sales");
-    
+
     struct Sale {
         uint32 startTime;
         uint32 endTime;
@@ -28,5 +28,25 @@ library LibSales {
         assembly {
             ss.slot := salesKey
         }
+    }
+
+    function setSale(uint256 saleId, Sale memory saleData) internal {
+        SalesStorage storage ss = salesStorage();
+        ss.sales[saleId] = saleData;
+    }
+
+    function getSale(uint256 saleId) internal view returns (Sale memory) {
+        SalesStorage storage ss = salesStorage();
+        return ss.sales[saleId];
+    }
+
+    function setSaleStats(uint256 saleId, address buyer, uint256 numBundles) internal {
+        SalesStorage storage ss = salesStorage();
+        ss.saleStats[saleId][buyer] += numBundles;
+    }
+
+    function getSaleStats(uint256 saleId, address buyer) internal view returns (uint256) {
+        SalesStorage storage ss = salesStorage();
+        return ss.saleStats[saleId][buyer];
     }
 }
