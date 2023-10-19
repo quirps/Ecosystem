6e933be4964f32733aca86e3cc7cb6eb854c791d
quirp <joesters@msn.com>
2023-10-18 19:00:25 -0700

    refactor parametere freeze

--- playground/ABIEncode.js
+++
@@ -1,5 +0,0 @@
-const {ethers} = require("ethers")
-
-let abiCoder = ethers.utils.defaultAbiCoder
-var data = abiCoder.encode(["uint256","address","address"], [429,"0xb79872DC1E960B7C6B9b5E832dD55D9c2bf653cb","0xDAFEA492D9c6733ae3d56b7Ed1ADB60692c98Bc5"]);
-console.log(data)
